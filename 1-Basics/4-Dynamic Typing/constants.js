707abb8bfea75f910056188dd3da0e11194c9e64
Ferdous Mikdad <mikdadtaqi2024@gmail.com>
2024-08-09 12:38:52 +0600

    Dynamic Typing

---
+++ 1-Basics/4-Dynamic Typing/constants.js
@@ -0,0 +1,23 @@
+
+// JavaScript is a dynamic language.  
+// What do I mean by dynamic?
+// we have two types of programming languages. 
+// Static languages or Dynamic language.
+
+//In static languages, when we declare a variable, the type of the variable is set and it cannot be changed
+
+//In dynamic languages like JS, the type of a variable can change at run time. Let's see this in the code.
+
+let name = 'Ferdous';
+let age = 26;  
+let isApproved = true; 
+let firstName = undefined;
+let selectedColor = null;
+
+//check these in console and type in the console like 
+// typeof name
+// You will get the answer "string"
+// If we set the value of name like "name = 1"
+// you will get the answer "number" when you will type in the console "type of name" after change the value of name
+
+// You can google or ChatGpt it if you don't understand better. 
