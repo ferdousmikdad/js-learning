faa1feb4dd9f655a06f89d95dee138291abecbeb
Ferdous Mikdad <mikdadtaqi2024@gmail.com>
2024-08-09 12:25:17 +0600

    Primitive Types

---
+++ 1-Basics/3-Primitive Types/constants.js
@@ -0,0 +1,26 @@
+
+//So you have learned how to declare and initialize a variable.
+//In JavaScript we have two categories of types.
+
+// Primitives/Value Types
+//On one side, we have primitives, also called Value Types
+
+//Reference Types. We will learn about it later in the course.
+
+//In the category of primitives,
+//We have String, Number, Boolean, Undefined and Null
+
+// Let's look each of these in action 
+
+let name = 'Ferdous';   //String Literal
+let age = 26;           //Number Literal
+let isApproved = true;  //Boolean Literal
+// So the value of a boolean variable can be true or false.
+let firstName = undefined;
+// let lastName = null;
+let selectedColor = null;
+
+// These are the example of primitive types. 
+//We have strings, numbers, booleans, undefined and null.
+
+//Now in ES6 we have another primitive that is symbol.
