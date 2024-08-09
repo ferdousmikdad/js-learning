da6a67375bcfaadd0f05b45ade28e67ae93d0c8e
Ferdous Mikdad <mikdadtaqi2024@gmail.com>
2024-08-09 18:45:26 +0600

    Functions

---
+++ 1-Basics/7-Functions/functions.js
@@ -0,0 +1,17 @@
+
+//Functions are on of the fundamental building blocks in JavaScript.
+//A function is basically a set of statements that performs a task or calculates a value. 
+//Let show you a couple examples. 
+
+// function greet() {
+//     console.log('Hello World');
+// }
+// greet();
+
+function person(name, lastName) {
+    console.log('Hello ' + name+ ' ' + lastName);
+}
+
+person('Ferdous', 'Mikdad');
+
+// Now a function can have multiple parameters.
