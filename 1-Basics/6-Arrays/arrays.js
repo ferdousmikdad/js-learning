dcb5174b708856e2b313169736e75199ad6f8e18
Ferdous Mikdad <mikdadtaqi2024@gmail.com>
2024-08-09 18:34:51 +0600

    Arrays

---
+++ 1-Basics/6-Arrays/arrays.js
@@ -0,0 +1,11 @@
+
+// Sometimes, we might be dealing with a list of objects.
+// For example, 
+
+let selectedColors = ['red', 'blue'];
+//the index of the first element is 0 as you can see in console.
+selectedColors[2] = 1;
+//Now, let's display this array on console.
+console.log(selectedColors.length);
+//So, we have an array with 3 
+//So the length is dynamic, it can change.
