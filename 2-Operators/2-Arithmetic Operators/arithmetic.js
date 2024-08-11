5fd90f7db4e6f1c8d8db2d1d6afd121e9812f584
Ferdous Mikdad <mikdadtaqi2024@gmail.com>
2024-08-11 23:45:32 +0600

    Arithmetic Operators

---
+++ 2-Operators/2-Arithmetic Operators/arithmetic.js
@@ -0,0 +1,35 @@
+
+//Arithmetic Operators
+
+let x = 10;
+let y = 3;
+
+console.log(x + y);
+//That is the edition operators
+//So this arithmetic operators usually take two up rands(?)
+//in this case x and y.
+//And then produce a new value.
+
+//An expression is something that produce a value. 
+//We also have... 
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+
+//We have a new operator in JS that is exponentiation
+//it's indicated by two stars, 
+
+console.log(x ** y);
+//and that is x to the power of y.
+
+
+//Tow other operators are 
+//1. Increment (++)
+console.log(++x);
+console.log(x++);
+console.log(x);
+
+
+//2. Decrement (--)
+console.log(--x);
