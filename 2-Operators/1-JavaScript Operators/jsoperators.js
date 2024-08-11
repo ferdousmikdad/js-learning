bce3f5e25011c896c46720ed5d312c0fb6262659
Ferdous Mikdad <mikdadtaqi2024@gmail.com>
2024-08-11 22:53:55 +0600

    Java Script Operators

---
+++ 2-Operators/1-JavaScript Operators/jsoperators.js
@@ -0,0 +1,15 @@
+
+//Operators
+//In JS we have different kinds of operators. 
+//Use these operators along with our variables
+//and constants to create expressions.
+//And with these expressions we can implement logic and algorithms
+
+//Different kinds of operators
+//1. Arithmetic
+//2. Assignment
+//3. Comparison
+//4. Logical
+//5. Bitwise
+
+//We will learn each of these in detail.
