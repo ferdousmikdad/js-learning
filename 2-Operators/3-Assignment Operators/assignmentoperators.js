9fa2e983ce5154a16139327d126c23b0db97d92d
Ferdous Mikdad <mikdadtaqi2024@gmail.com>
2024-08-11 23:55:34 +0600

    Assignment Operator

---
+++ 2-Operators/3-Assignment Operators/assignmentoperators.js
@@ -0,0 +1,20 @@
+
+//Assignment Operators
+
+let x = 10;
+
+// x++;
+// But what is in this example instead of adding 1 to x
+// We want to add 5.
+// well, this increment operator will not work. 
+
+x = x + 5;
+x += 5;
+//Line 11 and line 12 are exactly the same. 
+
+x = x * 3;
+x *= 3;
+
+// So all the other arithmetic operators we learned 
+// addition, subtraction, division, multiplication, and the remainder
+// all of this have a combination with the assignment operator.
