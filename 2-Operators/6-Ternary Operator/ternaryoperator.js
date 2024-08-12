e0428edf026580455e78e2205105d456824ae50c
Ferdous Mikdad <mikdadtaqi2024@gmail.com>
2024-08-12 23:37:07 +0600

    Ternary Operator

---
+++ 2-Operators/6-Ternary Operator/ternaryoperator.js
@@ -0,0 +1,15 @@
+
+// Ternary Operator or Conditional operator, 
+// which is on of my favorite operators in JS
+
+// If a customer has more than 100 points, 
+// they are a 'gold' customer, otherwise,
+// they are a 'silver' customer.
+
+let points = 110;
+let type = points > 100 ? 'gold' : 'silver';
+
+console.log(type);
+
+// You should try to change the number of points to 90 and see what you get.
+// Your answer will be 'silver'
