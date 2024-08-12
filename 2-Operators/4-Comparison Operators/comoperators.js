25f7f1ccc0557d8356acba0c9e8618112d8574b4
Ferdous Mikdad <mikdadtaqi2024@gmail.com>
2024-08-12 23:03:10 +0600

    Comparison Operators

---
+++ 2-Operators/4-Comparison Operators/comoperators.js
@@ -0,0 +1,18 @@
+
+// Comparison Operators
+
+let x = 1;
+
+// Relational
+console.log(x > 0);
+// Comparison operator is a boolean,
+// it's true or false.
+console.log(x >= 1);
+console.log(x < 1);
+// So this item is false, because x is not less than 1.
+console.log(x <= 1);
+
+// Equality
+console.log(x === 1);
+// So we have triple equal signs.
+console.log(x !== 1);
