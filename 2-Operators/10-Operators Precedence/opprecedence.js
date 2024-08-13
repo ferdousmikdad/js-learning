aa7e21f5d0841341b401708fac17510fea55c6b7
Ferdous Mikdad <mikdadtaqi2024@gmail.com>
2024-08-14 01:28:18 +0600

    Operators Precedence

---
+++ 2-Operators/10-Operators Precedence/opprecedence.js
@@ -0,0 +1,20 @@
+
+// Operators Precedence
+// When working with complex expression, 
+// one thing that you need to account for is the precedence of operators
+// example, 
+let x = 2 + 3 * 4;
+
+console.log (x);
+
+// Now, all these operators you have learned in this section, 
+// they have their own precedence.
+// It's really hard to memorize which operators have 
+// ---higher precedence or lower precedence.
+// So when working with complex expression you can use 
+// ---parenthesis to determine how these operators are applied.
+// For example, 
+
+ let y = (2 + 3) * 4;
+
+ console.log(y);
