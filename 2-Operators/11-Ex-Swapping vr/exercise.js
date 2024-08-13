efbd0f5f69a0662f7c3fd6545a6fc7a26c1e8053
Ferdous Mikdad <mikdadtaqi2024@gmail.com>
2024-08-14 01:55:20 +0600

    Exercise - Swapping Variables

---
+++ 2-Operators/11-Ex-Swapping vr/exercise.js
@@ -0,0 +1,18 @@
+
+// Exercise - Swapping Variables
+
+// Let's declare
+
+let a = 'red';
+let b = 'blue';
+
+let c = a;
+a = b;
+b = c;
+
+console.log(a);
+console.log(b);
+
+// Now if you couldn't do this exercise, don't worry, 
+// this is because you have not trained your programming brain.
+// And this requires a bit of time. 
