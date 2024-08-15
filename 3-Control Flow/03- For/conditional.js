6acde811420c7eb53df3698e8305f2ed441f35e7
Ferdous Mikdad <mikdadtaqi2024@gmail.com>
2024-08-15 21:53:03 +0600

    For

---
+++ 3-Control Flow/03- For/conditional.js
@@ -0,0 +1,21 @@
+
+// For
+// Sometimes we want to repeat an action a number of times, 
+// For example, let's imagine 
+// we want to display "hello world" 5 times on the console.
+
+// LOOPS
+// In JavaScript we have various kinds of loops, 
+
+// 1- For loops,
+// 2- While loops
+// 3- Do While loops
+// 4- For...in loops
+// 5- For...of loops
+
+// 1- For Loops
+
+for (let i = 1; i <= 5; i++) {
+    console.log('Hello World', i);
+    // if (i % 2 !== 0) console.log(i);
+}
