1a7e56b6d51330f11d90023f9df6574e0e68bf0c
Ferdous Mikdad <mikdadtaqi2024@gmail.com>
2024-08-15 22:03:34 +0600

    While Loops

---
+++ 3-Control Flow/04- While loops/whileloops.js
@@ -0,0 +1,13 @@
+
+// While Loops
+// One key different between a 'While loops' and 'For loops'
+// is that in for loop variable is part of the loop variable itself
+// But in while loops, 
+// you have to declare this while variable externally.
+
+
+let i = 0;
+while (i <= 5) {
+    if (1 % 2 !== 0) console.log(i);
+    i++;
+}
