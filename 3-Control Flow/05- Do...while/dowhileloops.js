d4f5107763ef110583abb62c9cb906e9e5bca9b2
Ferdous Mikdad <mikdadtaqi2024@gmail.com>
2024-08-16 12:56:27 +0600

    Do...While Loops

---
+++ 3-Control Flow/05- Do...while/dowhileloops.js
@@ -0,0 +1,24 @@
+
+// Do...While 
+// for (let i = 0; i <= 5; i++) {
+//     if (i % 2 !== 0) console.log(i);
+// }
+
+// let i = 0;
+// while (i <= 5) {
+//     if (i % 2 !== 0) console.log(i);
+//     i++;
+// }
+
+// do-while
+
+let i = 9;
+do {
+    if (i % 2 !== 0) console.log(i);
+    i++;
+} while (i <= 5);
+
+// What is the difference between a while loop, and a do-while loop?
+// * do-while loops are always executed at least once.
+// Even if this condition evaluates to false.
+
