aa339bd3e9de27a305bc209406d809557451b474
Ferdous Mikdad <mikdadtaqi2024@gmail.com>
2024-08-19 23:34:45 +0600

    Break and Continue

---
+++ 3-Control Flow/09- Break and continue/brandcontinue.js
@@ -0,0 +1,15 @@
+ 
+
+// Break and Continue
+
+let i = 0;
+while (i <= 10) {
+    // if (i === 5) break;
+
+    if (i % 2 === 0) {
+        i++;
+        continue;
+    }
+    console.log(i);
+    i++;
+}
