57aa948c6089b4aed2cf53248644132562fb6082
Ferdous Mikdad <mikdadtaqi2024@gmail.com>
2024-08-19 23:25:40 +0600

    For-in Loops

---
+++ 3-Control Flow/07- For...in/loops.js
@@ -0,0 +1,30 @@
+
+// So far you learned about 3 kinds of loops in JavaScript
+// for
+// while
+// do-while
+
+// With all these loops we can repeat an action a number of times.
+
+// We have two more loops
+
+// For-in
+
+const person = {
+    name: 'Mikdad',
+    age: 30
+};
+
+for (let key in person)
+    console.log(key, person [key]);
+
+// Dot Notation
+// person.name
+
+// Bracket Notation
+// person['name']
+
+const colors = ['red' , 'green' , 'blue'];
+
+for (let index in colors)
+    console.log(index, colors[index]);
