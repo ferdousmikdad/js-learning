700a0d3700460f04d96ce0455ce429ff09dcacbf
Ferdous Mikdad <mikdadtaqi2024@gmail.com>
2024-08-21 23:23:19 +0600

    Exercise-02

--- 3-Control Flow/11. Exercise-2/exercise.js
+++ 3-Control Flow/11. Exercise-2/exercise.js
@@ -1,11 +1,8 @@
 
-// Write a function that takes two numbers and <br />
-// and returns the maximum of the two.
-// Call that function, give a different argument
-// and make sure it's working properly
-let number = max(5, 10);
-console.log(number);
+// Exercise - 02
 
-function max(a, b) {
-    return (a > b) ? a : b;
+console.log(isLandscape (800, 600));
+
+function isLandscape(width, height) {
+    return (width > height);
 }
