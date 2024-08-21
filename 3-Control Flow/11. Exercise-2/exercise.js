a2adb411dc471030422665ec5ac139b6a9d45e3b
Ferdous Mikdad <mikdadtaqi2024@gmail.com>
2024-08-21 23:16:08 +0600

    Exercise-2 setup done

---
+++ 3-Control Flow/11. Exercise-2/exercise.js
@@ -0,0 +1,11 @@
+
+// Write a function that takes two numbers and <br />
+// and returns the maximum of the two.
+// Call that function, give a different argument
+// and make sure it's working properly
+let number = max(5, 10);
+console.log(number);
+
+function max(a, b) {
+    return (a > b) ? a : b;
+}
